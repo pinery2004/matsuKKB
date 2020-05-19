ee417fa185f21de7b3e9576d62d0f4c2877fc1b1
Kenji <pinery2004@yahoo.co.jp>
2020-05-19 10:28:00 +0900

    initial 2

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MatsuKKB
+{
+	/// <summary>
+	/// App.xaml の相互作用ロジック
+	/// </summary>
+	public partial class App : Application
+	{
+		public static readonly string dataName = "啓子";
+		public static readonly string processingName = "入力一覧";
+		public static readonly string ver = "1.1";
+	}
+}
---
+++ CDaily.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatsuKKB
+{
+	class CDaily
+	{
+		/// 選択されたタブの情報を表示する
+		//private void DayListTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		//{
+		//	// 選択されタブの番号を取得
+		//	int selectedIndex = DayListTabControl.SelectedIndex + 1;
+
+		//	// 選択された他部の情報を表示
+		//	DayListTabControl.Text = selectedIndex.ToString() + "番目のタブが選択されました";
+		//}
+	}
+}
